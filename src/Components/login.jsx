0a56d5f6499b2b582e65e93feab4b4567017181e
Jose Cobas <julgranen7@gmail.com>
2021-05-17 11:24:59 +0200

    email

--- src/Components/login.jsx
+++ src/Components/login.jsx
@@ -3,6 +3,16 @@ import React from 'react'
 function login() {
   return (
     <div>
+        <form>
+            <div>
+                <label htmlFor="inputEmail">Email adress</label>
+                <input type="email"
+                       className="form-control"
+                       id="email"
+                       placeholder="Enter email"
+                />
+            </div>
+        </form>
       
     </div>
   )
