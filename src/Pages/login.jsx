ca15732054b40ab996b42152617a45518a01ffdf
Jose Cobas <julgranen7@gmail.com>
2021-05-17 15:48:13 +0200

    change name

--- src/Pages/login.jsx
+++ src/Pages/login.jsx
@@ -1,39 +1,9 @@
 import React from 'react'
-import '../Components/CSS/login.css'
+
 
 function login() {
   return (
-    <div className="general">
-        <form >
-
-          <h1>Register</h1>
-            <div className="divar">
-                <label htmlFor="inputEmail"
-                       className="label">Email adress</label> <br />
-                <input type="email"
-                       placeholder="Enter email"
-                       className="input"
-                />
-            </div>
-            <div className="divar">
-              <label htmlFor="inputpasword"
-                     className="label">Password</label> <br/>
-              <input type="password"
-                     placeholder="Password"
-                     className="input"
-              />                              
-            </div>
-            <div className="divar">
-              <label htmlFor="inputPasswordConfirmation"
-                     className="label">Confirm</label> <br />
-              <input type="password"
-                     placeholder="Confirm password"
-                     className="input"
-              />
-            </div>
-            <button type="submit"
-                    className="btn">Register</button>
-        </form>
+    <div>
       
     </div>
   )
