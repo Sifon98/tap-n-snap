db6cabe6e36c076063cbce6edc5e7c834693f89c
Jose Cobas <julgranen7@gmail.com>
2021-05-17 15:47:39 +0200

    changed login to register

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import Navbar from './Components/Navbar'
 import login from './Pages/login'
+import register from './Pages/register'
 import {
   BrowserRouter as Router,
   Switch,
@@ -16,6 +17,7 @@ function App() {
         <Switch>
           <Route exact path='/' component={Navbar}/>
           <Route exact path='/login' component={login}/>
+          <Route exact path='/register' component={register}/>
         </Switch>
       </Router>
     </div>
