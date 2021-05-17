59aa83dd02e8c8edf151194008a684ebc6e21bea
Jose Cobas <julgranen7@gmail.com>
2021-05-17 16:29:07 +0200

    html i login page

--- src/Pages/login.jsx
+++ src/Pages/login.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import '../Components/CSS/login.css'
 
 
 function login() {
   return (
     <div>
-      
+      <h1>Login</h1>
+      <div className="divar">
+        <label htmlFor="inputEmail"
+               className="label">Email adress</label>
+               <input type="email"
+                      placeholder="Enter email"
+                      className="input"
+               />
+      </div>
+      <div className="divar">
+        <label htmlFor="inputpassword"
+               className="label">Password</label>
+        <input type="password"
+               placeholder="Password"
+               className="input"
+        />
+      </div>
+
+      <button type="submit"
+              className="btn">Submit</button>
     </div>
   )
 }
