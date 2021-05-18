4d4cd46985112f0dc7e850cc75076544fc9de716
Abdul Muqeet <abdul.khan@live.se>
2021-05-18 11:52:24 +0200

    Searchbar + search icon done

--- src/Components/Searchbar.jsx
+++ src/Components/Searchbar.jsx
@@ -5,14 +5,36 @@ import TestImage from '../img/TestImage.jpg'
 function Searchbar() {
     return (
         <div>
-            <input type="text" className={Style.searchBar} placeholder ="Search..."></input>
-            <button className ={Style.searchBarButton}>Search</button>
+            <div className={Style.formSection}>
+                <form>
+                    <input type="search" className={Style.searchBar} placeholder ="Search for photos">
+                    </input>
+
+                    <button type="submit" className ={Style.submitButton}>
+                        <i class="fa fa-search"></i>
+                    </button>
+                    
+                    {/* <button type="reset" className ={Style.clearButton}>
+                        <i class="fa fa-search"></i>
+                    </button> */}
+
+                </form>
+            </div>
 
             <div className={Style.imgContainer}>
                 <img src={TestImage} className={Style.images}></img>
             </div>
         </div>
     )
+
+    // <div>
+    //         <input type="text" className={Style.searchBar} placeholder ="Search..."></input>
+    //         <button className ={Style.searchBarButton}>Search</button>
+
+    //         <div className={Style.imgContainer}>
+    //             <img src={TestImage} className={Style.images}></img>
+    //         </div>
+    //     </div>
     
 }
 
