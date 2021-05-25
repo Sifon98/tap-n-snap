0b42385bdb1a454abf873fd08a61db121dac3fd4
Jose Cobas <julgranen7@gmail.com>
2021-05-25 22:18:21 +0200

    klar med iconer

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Style from '../Components/CSS/register.module.scss'
 import Navbar from '../Components/Navbar'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faUser} from '@fortawesome/free-solid-svg-icons'
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import {faKey} from '@fortawesome/free-solid-svg-icons'
 
 function Register() {
   return (
@@ -9,20 +13,24 @@ function Register() {
            <br />
            <br />
        <Navbar />
+       <FontAwesomeIcon icon={faUser} className={Style.faicon}/> 
        <input type="username"
-               placeholder="Username..."
+               placeholder="  Username..."
                className={Style.inputr1} />
                <br />
+        <FontAwesomeIcon icon={faEnvelope} className={Style.faicon}/>
         <input type="email"
-               placeholder="Email..."
+               placeholder="  Email..."
                className={Style.inputr} />       
                <br />
+        <FontAwesomeIcon icon={faKey} className={Style.faicon}/>       
         <input type="password"
-               placeholder="Password..."
+               placeholder="  Password..."
                className={Style.inputr} />
                <br />
+        <FontAwesomeIcon icon={faKey} className={Style.faicon}/>       
         <input type="password"
-               placeholder="Confirm Password..."
+               placeholder="  Confirm Password..."
                className={Style.inputr} />
                <br />
 
--- src/Pages/login.jsx
+++ src/Pages/login.jsx
@@ -2,29 +2,37 @@ import React from 'react'
 import Style from '../Components/CSS/login.module.scss'
 import Navbar from '../Components/Navbar'
 import { Link } from 'react-router-dom'
+import userLogo from '../img/user.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faUser} from '@fortawesome/free-solid-svg-icons'
+import {faKey} from '@fortawesome/free-solid-svg-icons'
+
 
 function Login() {
   return (
     <div>
-
-          <br />
-      <Navbar />
-      <img src="../img/user.png"/>
-      <input type="email"
-             placeholder="Username..."
-             className={Style.inputl}
-                 />
-          <br />
+        <br />
+        <Navbar />
+    
+        <FontAwesomeIcon icon={faUser} className={Style.faicon}/>  
+        <input type="email"          
+                placeholder="  Username... " 
+                className={Style.inputl}
+        />
+      
+        <br />
+        <FontAwesomeIcon icon={faKey} className={Style.faicon}/>
         <input type="password"
-                        placeholder="Password..."
-                        className={Style.input}
-                 />
-          <br />
+                placeholder="  Password..."
+                className={Style.input}
+          />
+    
+        <br />
         <button type="submit"         
                 className={Style.btnl}>SIGN IN</button>
-          <br />
+        <br />
 
-      <small>Don't have an account?</small> <Link className={Style.a}  to='/register'>Register</Link>     
+        <small>Don't have an account?</small> <Link className={Style.a}  to='/register'>Register</Link>     
     </div>
   )
 }
