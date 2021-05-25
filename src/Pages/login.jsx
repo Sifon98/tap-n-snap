2703ecc155f8f0e0abcb46eecb860c6cfbe7e7cd
Jose Cobas <julgranen7@gmail.com>
2021-05-25 22:28:40 +0200

    lets see

--- src/Pages/login.jsx
+++ src/Pages/login.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import Style from '../Components/CSS/login.module.scss'
+import style from '../Components/CSS/login.module.scss'
 import Navbar from '../Components/Navbar'
 import { Link } from 'react-router-dom'
-import userLogo from '../img/user.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faUser} from '@fortawesome/free-solid-svg-icons'
 import {faKey} from '@fortawesome/free-solid-svg-icons'
@@ -14,25 +13,25 @@ function Login() {
         <br />
         <Navbar />
     
-        <FontAwesomeIcon icon={faUser} className={Style.faicon}/>  
+        <FontAwesomeIcon icon={faUser} className={style.faicon}/>  
         <input type="email"          
                 placeholder="  Username... " 
-                className={Style.inputl}
+                className={style.inputl}
         />
       
         <br />
-        <FontAwesomeIcon icon={faKey} className={Style.faicon}/>
+        <FontAwesomeIcon icon={faKey} className={style.faicon}/>
         <input type="password"
                 placeholder="  Password..."
-                className={Style.input}
+                className={style.input}
           />
     
         <br />
         <button type="submit"         
-                className={Style.btnl}>SIGN IN</button>
+                className={style.btnl}>SIGN IN</button>
         <br />
 
-        <small>Don't have an account?</small> <Link className={Style.a}  to='/register'>Register</Link>     
+        <small>Don't have an account?</small> <Link className={style.a}  to='/register'>Register</Link>     
     </div>
   )
 }
