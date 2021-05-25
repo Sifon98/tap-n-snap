7b6f246d8ae10bcc989112e15e38771902c67853
Mudassar Khan <mudassar_khan@hotmail.se>
2021-05-25 11:30:43 +0200

    Co-authored-by: Akhan91 <Akhan91@users.noreply.github.com>

--- src/Components/CameraComponent.jsx
+++ src/Components/CameraComponent.jsx
@@ -1,37 +1,20 @@
-import React from 'react'
-
-export default function CameraComponent() {
-    return (
-
-
-        <div>
-            
-        <video id="player" autoplay></video>
-        <canvas id="canvas" width="320px" height="240px"></canvas>
-        <br/>
-        <button id="capture-btn">Capture</button>
-        <button id="new-capture-btn">Retake picture</button>
-
-        <div id="pick-image">
-            <h6>Pick an Image instead</h6>
-            <input type="file" accept="image/*" id="image-picker"/>
-        </div> 
-
-        <br/>
-        <button id="location-btn">Get Location</button>
-        <div id="location-display"></div>
-
-        <br/>
-        <button id="post-btn">Post image</button>
-
-        <br/>
-        <p>Saved picture:</p>
-        <img id="saved-picture" />
-
-            <script src="/src/Components/camera.js"></script>
+import React, { useState, useRef } from "react";
+import {Camera} from "react-camera-pro";
+// import Style from './CSS/camera.module.scss'
 
 
-        </div>
-        
-    )
+
+const CameraComponent = () => {
+  const camera = useRef(null);
+  const [image, setImage] = useState(null);
+ 
+  return (
+    <div>
+      <Camera ref={camera} />
+      <button onClick={() => setImage(camera.current.takePhoto())}>Take photo</button>
+      <img src={image} alt='Taken photo'/>
+    </div>
+  );
 }
+ 
+export default CameraComponent;
