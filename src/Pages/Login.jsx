ab5cce5bf6c2a00df793670aeca1f7d0bd268d8c
Alexander Gustafsson <alegus753@gmail.com>
2021-05-26 10:35:26 +0200

    Added Login Again

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import style from '../Components/CSS/login.module.scss'
+import Navbar from '../Components/Navbar'
+import { Link } from 'react-router-dom'
+
+
+function Login() {
+  return (
+    <div>
+        <br />
+        <Navbar />
+           
+        <i className="fas fa-user" ></i>
+        <input type="email"          
+                placeholder="  Username... " 
+                className={style.inputl}
+        />
+      
+        <br />
+        
+        <i className="fas fa-key" ></i>
+        <input type="password"
+                placeholder="  Password..."
+                className={style.input}
+          />
+    
+        <br />
+        <button type="submit"         
+                className={style.btnl}>SIGN IN</button>
+        <br />
+
+        <small>Don't have an account?</small> <Link className={style.a}  to='/register'>Register</Link>     
+    </div>
+  )
+}
+
+export default Login
