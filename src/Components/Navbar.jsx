7a751312421ed022fd67eafcd7616f3d33294066
Alexander Gustafsson <alegus753@gmail.com>
2021-05-27 16:28:33 +0200

    Redesigned, added animations to footer and header

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,29 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Style from './CSS/navbar.module.scss'
 import Logo from '../img/logo.png'
-import { Link } from 'react-router-dom'
 
-var back = "";
-var profile = "";
+const Navbar = () => {
+    const [visible, setVisible] = useState(false)
 
-function Navbar({ backBtn, profileBtn }) {
-    
-    if (backBtn == true)
-        back = <i className="fas fa-chevron-left fa-2x"></i>
-    else 
-        back = ""
-    
-    if (profileBtn == true)
-        profile = <i className="fas fa-user fa-2x"></i>
-    else 
-        profile = ""
-    
+    useEffect(() => {
+        var listener = document.addEventListener("scroll", e => {
+            const scrolled = document.documentElement.scrollTop;
+            if (scrolled > 100){
+              setVisible(true)
+            } 
+            else if (scrolled <= 100){
+              setVisible(false)
+            }
+        })
+
+        return () => {
+          document.removeEventListener("scroll", listener)
+        }
+    }, [visible])
+
     return (
-        <div className={Style.navbar}>
-            <Link className={Style.left} to="/">{back}</Link>
+        <nav className={`${ visible ? Style.navbarSmall : Style.navbar}`}>
             <img src={Logo} alt="logo"/>
-            <Link className={Style.right} to="/profile">{profile}</Link>
-        </div>
+            {/* <FaArrowCircleUp onClick={scrollToTop} /> */}
+        </nav>
     )
 }
 
